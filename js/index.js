576cac519f86c74b7ebd63e9a0289dc0e66b0a3f
rishiraj-dhar <rishiraj1203@gmail.com>
2021-11-14 02:08:58 +0530

    Add symbol rendering on click

--- js/index.js
+++ js/index.js
@@ -1,3 +1,48 @@
+let turn = 0;
+
+class Player {
+    isMarkedCell(row, col) {
+        return this.occupancyGrid[row][col] == 1;
+    }
+
+    markCell(row, col) {
+        this.occupancyGrid[row][col] = 1;
+    }
+
+    getSymbol() {
+        return this.symbol;
+    }
+
+    constructor(symbol) {
+        this.symbol = symbol;
+        this.occupancyGrid = [  [0, 0, 0], 
+                                [0, 0, 0],
+                                [0, 0, 0]   ];
+    }
+};
+
+let player = [];
+
+player.push(new Player('X'));
+player.push(new Player('O'));
+
+const checkCell = (row, col) => {
+    if (!(player[0].isMarkedCell(row, col) || player[1].isMarkedCell(row, col))) {
+        player[turn].markCell(row, col);
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const clickOnCell = (row, col) => {
+    if (checkCell(row, col)) {
+        let clickedCell = document.getElementById(`cell-${row}${col}`);
+        clickedCell.innerText = player[turn].getSymbol();
+        turn = 1 - turn;
+    }
+};
+
 window.onload = (loadEvent) => {
     let gameBlock = document.getElementsByClassName("game-block")[0];
     for (let i = 0; i < 3; i++) {
@@ -17,7 +62,9 @@ window.onload = (loadEvent) => {
             if (j != 0) {
                 cellDiv.classList.add("cell-border-left");
             }
-            // cellDiv.innerText = "X";
+
+            clickFunctionString = `clickOnCell(${i}, ${j})`;
+            cellDiv.setAttribute("onclick", clickFunctionString);
             gameBlock.appendChild(cellDiv);
         }
     }
